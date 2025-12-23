3c7ca3db8f79c2f13822da0e727e6cffde1a5059
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-23 21:44:19 +0800

    [firmware] rebranding to axion
    Change-Id: I9d2ab5fe4011cb536cc0cf81f981f6dddd9c0ca4
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -9,7 +9,7 @@ PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
-PRODUCT_BRAND ?= LineageOS
+PRODUCT_BRAND ?= AxionOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PRODUCT_PROPERTIES += \

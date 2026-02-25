d59991716ca20ba5a8b2531a76a448c4df086864
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-02-25 09:19:53 +0800

    [defaults] adding tflite common deps
    Change-Id: I9ff69886c239f5897f8a5e09c4d7cf863056a035

--- config/defaults_common.mk
+++ config/defaults_common.mk
@@ -1,6 +1,8 @@
 TARGET_PRODUCT_PROP += \
     vendor/lineage/config/defaults_common.prop
 
+$(call inherit-product-if-exists, axion_sdk/ax_tflite/common.mk)
+
 PRODUCT_PACKAGES += \
     AxThemePicker \
     AxQuickLook

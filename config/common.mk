0691c0a56403a479536d3ebad40e725521b36de4
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-27 18:29:26 +0800

    making setupwizard props overridable
    Change-Id: I0f7777b385fd7fd941297f453cfa83aa84ad7159

--- config/common.mk
+++ config/common.mk
@@ -270,8 +270,8 @@ $(call inherit-product, vendor/lineage/audio/audio.mk)
 
 # SetupWizard
 PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.theme=glif_v4 \
-    setupwizard.feature.day_night_mode_enabled=true
+    setupwizard.theme?=glif_v4 \
+    setupwizard.feature.day_night_mode_enabled?=true
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \
